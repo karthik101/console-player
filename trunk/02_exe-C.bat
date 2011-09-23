321916f3d17059938d724047b7effe4fd953231e
kilik929@gmail.com <kilik929@gmail.com@74f8f1bc-e4c4-4f01-21ac-82387f9397a1>
2011-09-23 20:58:57 +0000

    removing non-project files

--- trunk/02_exe-C.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-"C:\Python27\python.exe" "C:\Python27\Lib\site-packages\pyInstaller\Build.py" "D:\Dropbox\Scripting\PyQt\ConsolePlayer\src\Console.spec"
--- trunk/exec.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-
-cd /home/nick/windows/D_DRIVE/Dropbox/Scripting/PyQt/ConsolePlayer/src
-
-python /home/nick/PyPackage/pyinstaller-1.5.1/Build.py ConsolePlayer.spec
