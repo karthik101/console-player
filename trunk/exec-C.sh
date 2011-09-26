e46e3331205705c19320654261dcf89ec2c48c8b
kilik929@gmail.com <kilik929@gmail.com@74f8f1bc-e4c4-4f01-21ac-82387f9397a1>
2011-09-26 03:29:20 +0000

    Finally Have the Commit/File Structure figured out

--- trunk/exec-C.sh
+++ trunk/exec-C.sh
@@ -2,4 +2,6 @@
 
 cd /home/nick/windows/D_DRIVE/Dropbox/Scripting/PyQt/ConsolePlayer/src
 
+export PYTHONPATH="/home/nick/windows/D_DRIVE/Dropbox/Scripting/PyModule/GlobalModules/src"
+
 python /home/nick/PyPackage/pyinstaller-1.5.1/Build.py Console.spec
--- trunk/exec.sh
+++ trunk/exec.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 
-cd /home/nick/windows/D_DRIVE/Dropbox/Scripting/PyQt/ConsolePlayer/src
+cd "/home/nick/windows/D_DRIVE/Dropbox/Scripting/PyQt/console-player/src/"
 
-python /home/nick/PyPackage/pyinstaller-1.5.1/Build.py ConsolePlayer.spec
+export PYTHONPATH="/home/nick/windows/D_DRIVE/Dropbox/Scripting/PyModule/GlobalModules/src"
+
+#python /home/nick/PyPackage/pyinstaller-1.5.1/Build.py ConsolePlayer.spec
