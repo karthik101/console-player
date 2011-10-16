bcc3c32267372899b8fa29265d147f3f8bfd0c3a
kilik929 <kilik929@74f8f1bc-e4c4-4f01-21ac-82387f9397a1>
2011-10-16 13:39:10 +0000

    fixed issue with loading settings

--- trunk/src/VersionController.py
+++ trunk/src/VersionController.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python -O
-__VERSION__ = "0.4.3.185"           
+__VERSION__ = "0.4.3.203"           
 
 # ################################################
 # do not edit anything above this line unless you 
