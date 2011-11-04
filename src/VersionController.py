c861aeeac6d217d2db6b475a25a928e40fee5f17
kilik929 <kilik929@74f8f1bc-e4c4-4f01-21ac-82387f9397a1>
2011-11-04 23:49:08 +0000

    bug fixes and improved support for file explorer.

--- src/VersionController.py
+++ src/VersionController.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python -O
-__VERSION__ = "0.4.4.102"           
-
+__VERSION__ = "0.4.4.108"           
  ################################################
 # do not edit anything above this line unless you 
 # know what you are doing
