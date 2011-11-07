ce79bd7c6893a586234876482709814d23ef4f56
kilik929 <kilik929@74f8f1bc-e4c4-4f01-21ac-82387f9397a1>
2011-11-07 02:52:04 +0000

    updates to imports in module files

--- trunk/module/Song_LibraryFormat.py
+++ trunk/module/Song_LibraryFormat.py
@@ -303,3 +303,64 @@ def LIBZ_process_block(data,typ,fmt, drivelist):
         R.append( Song( repr ) );
     
     return R;
+
+# ###########################  
+    
+def musicBackup(path,songList,format=0,force = False):
+    """
+        save a copy of the current library to ./backup/
+        only save if one has not been made today,
+        delete old backups
+        
+        adding new music, deleting music, are good reasons to force backup
+    """
+    #path = os.path.join(MpGlobal.installPath,'backup')
+    #songList = MpGlobal.Player.library
+    
+    if not os.path.exists(path):
+        os.mkdir(path)
+        print "backup directory created"
+        
+    datetime = time.localtime(time.time())
+    date,hourmin = time.strftime("%Y/%m/%d %H:%M",datetime).split(' ')
+    date = date.replace('/','-').replace('\\','-')
+    
+    name = 'music_backup-'
+    fullname = name+date+'.libz'
+    h,m = hourmin.split(':')
+        
+    #if (atoi(h) > 12) or force:  # save backups in the afternoon only
+    
+    R = []
+    dir = os.listdir(path)
+    for file in dir:
+        if file.startswith(name) and fileGetExt(file) == "libz":
+            R.append(file);
+            
+    newestbu = ""
+
+    R.sort(reverse=True)
+    
+    if len(R) > 0:
+
+        #remove old backups
+        # while there are more than 6, 
+        # and one has not been saved today
+        while len(R) > 6 and R[0] != fullname: 
+            delfile = R.pop()
+            print "Deleting %s"%delfile
+            os.remove(os.path.join(path,delfile))
+            
+        # record name of most recent backup, one backup per day unless forced    
+        newestbu = R[0]
+        
+    # save a new backup 
+    if force or newestbu != fullname:
+        print "Saving %s"%fullname
+        newbu = os.path.join(path,fullname)  
+        musicSave_LIBZ(newbu,songList,format);
+         
+    
+    
+    
+    
