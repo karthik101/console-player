6f53b71cc45a2e39e491e1a02629e329b0a7f9e2
kilik929 <kilik929@74f8f1bc-e4c4-4f01-21ac-82387f9397a1>
2012-02-13 01:02:25 +0000

    updates to modules foler

--- module/Song_PlaylistFormat.py
+++ module/Song_PlaylistFormat.py
@@ -1,10 +1,44 @@
 
+# NOTE:
+# this file currently supports reading / writing of M3U files
+# this is the new paylist format
+# for legacy reasons, it allows reading of the old style playlist format
+# if an old style format is used, the following functions will automatically handle the
+# conversion  to/from the old and new styles. 
+
+# format for binary search:
+# for each song generate the following tuple, and store in an array
+# ( Song, Song[PATH].lower().reverse() )  # any other path operation found in the compare_path function should be added here as well.
+# sort by the reversed path.
+# when searching perform a similar operation to the input path, then binary search for the song
 
 from SystemPathMethods import *
 from Song_Object import *
 from datatype_hex64 import *
+from SystemDateTime import DateTime
+
+import codecs
+
+# sort the library given to playlist load
+# use a binary search to find the songs
+# for 4096 songs in the library. a linear search for a playlist of 50 songs is:
+#       50*4096 = 204800 comparisons  ~200k
+# for a binary search it is:
+#       50*12 = 600 comparisons
+# plus NlogN overhead from sort= 4096*12 = 49152 ~ 50k
+# it can be estimated that a binary search will be 4 times faster
+# sort time >> search time
 
 def playListSave(filepath,data,typ=0,index=0):
+
+    ext = fileGetExt(filepath)
+    
+    if ext == 'playlist':
+        filepath = filepath[:-8]+'m3u'
+    
+    playList_Save_M3U(filepath,data,typ)
+
+def playList_Save_oldstyle(filepath,data,typ=0,index=0): 
     # index = MpGlobal.Player.CurrentIndex
     driveList = systemDriveList()
     wf = open(filepath,"w")
@@ -16,8 +50,25 @@ def playListSave(filepath,data,typ=0,index=0):
             path = stripDriveFromPath(driveList,path)
         wf.write( "%s %s\n"%(data[x].id, unicode(path).encode('unicode-escape') ) )
     wf.close()
-    
+
 def playListLoad(filepath,source):
+
+    ext = fileGetExt(filepath)
+    
+    new_path = ""
+    
+    if ext == 'playlist':
+        new_path = filepath[:-8]+'m3u'
+    if ext == 'm3u':
+        new_path = filepath
+    
+    if new_path != "" and os.path.exists(new_path):
+        return [0,playList_Load_M3U(new_path,source)]
+            
+    return playList_Load_oldstyle(filepath,source)
+
+
+def playList_Load_oldstyle(filepath,source):
     
     if not os.path.exists(filepath):
         return (0,[]) # return empty array, then check status from that
@@ -114,5 +165,207 @@ def playListLoad(filepath,source):
     rf.close()
     
     return (last_index,array)    
+
+def playList_Save_M3U(filepath,data,typ=0): 
+
+    driveList = systemDriveList()
     
+    wf = codecs.open(filepath,"w",encoding='utf-8')
+
+    dt = DateTime();
+    dt.timer_start()
+    
+    for x in range(len(data)):
+    
+        path = data[x][EnumSong.PATH]
+        
+        if typ > 0:#alternate save formats remove the drive
+            path = stripDriveFromPath(driveList,path)
+            
+        wf.write( "%s\n"%( unicode(path) ) )
+        
+    wf.close()
+    
+    dt.timer_end();
+    print "Saved %d songs to m3u container in %s"%( len(data), DateTime.formatTimeDeltams(dt.timedelta))
+    
+def playList_Load_M3U(filepath,library):
+    """
+        load an M3U playlist, the file paths in the the meu file will be converted
+        to a list of Song objects
+        
+        filepath - path to the file to load
+        library - a list of songs already in memory
+    """
+    dt = DateTime();
+    dt.timer_start()
+
+    if not os.path.exists(filepath):
+        return [] 
+        
+    try:
+        rf = codecs.open(filepath,"r",encoding='utf-8')
+    except:
+        print "cannot open m3u file:\n   %s"%filepath
+        return [];
+     
+    bsearch = SongPath_Lookup(library) 
+     
+    playlist = []
+
+    line = rf.readline()
+    
+    count = 0
+    
+    while line:
+
+        try:
+        
+            line = line.strip()
+            
+            if line[0] == '#':
+                line = rf.readline() # load the next line ( a file path?) 
+                continue
+
+            song = bsearch.find(line)#path_to_song(line,library)    
+                
+            if song != None:
+                playlist.append(song)
+                
+        except Exception as e:
+            print "\n\n\nerror\n\n\n"
+            print e
+        finally:
+            
+            line = rf.readline()
+            count += 1;
+                
+    rf.close()
+    
+    dt.timer_end();
+    print "Loaded %d songs from m3u container in %s"%( len(playlist), DateTime.formatTimeDeltams(dt.timedelta))
+
+    return playlist
+    
+def path_to_song(path,library):
+
+    for song in library:
+        if song[EnumSong.PATH].endswith( path ):
+            return song
+            
+    return None
+
+    
+class SongPath_Lookup(object):
+    """
+        match a partial path to a Song from a given library
+        of songs.
+    """
+    def __init__(self,library):
+        
+        self.lib = [None] * len( library )
+        
+        # create a list of ( song , reverse path ) objects
+        for i in range(len(library)):
+        
+            song = library[i]
+            path = song[EnumSong.PATH]
+            
+            path = self.convert_path(path)
+            
+            self.lib[i] = ( song , path )
+            
+            #print self.lib[i]
+            
+        g = lambda x : x[1]
+
+        self.lib.sort(key=g)
+    
+    def convert_path(self,path):
+        # lowercase, stick correct, and reverse the path
+        return path.lower().replace("\\","/")[::-1]
+    
+    def find(self,path):
+        """ implement a special case binary search
+            a Song object will be returned if the following is true:
+                song[EnumSong.PATH].endswith(path)
+        """
+        
+        path = self.convert_path(path)
+        
+        # now find a path that starts with 'path' in the  
+        s,e = 0,len(self.lib)-1
+
+        while s <= e:
+            
+            i = (s+e) / 2
+            
+            item = self.lib[i][1]
+            
+            #print s,i,e, item, path
+            
+            if item.startswith(path):
+                 return self.lib[i][0]      
+            
+            if   path < item:
+                e = i - 1
+            elif path > item:
+                s = i + 1   
+            
+        return None
+          
+     
+
+if __name__ == "__main__":
+    lib = [
+            {EnumSong.PATH: "abc"}, \
+            {EnumSong.PATH: "def"}, \
+            {EnumSong.PATH: "ghi"}, \
+            {EnumSong.PATH: "jkl"}, \
+            {EnumSong.PATH: "mno"}, \
+            {EnumSong.PATH: "pqr"}, \
+            {EnumSong.PATH: "stu"}, \
+            {EnumSong.PATH: "vwx"}, \
+    
+          ]
+    
+    
+
+    
+    bsearch = SongPath_Lookup(lib)
+        
+    for item in lib:
+        print item[EnumSong.PATH],bsearch.find( item[EnumSong.PATH] )
+        
+    lib2 = [
+            {EnumSong.PATH: "bc"}, \
+            {EnumSong.PATH: "ef"}, \
+            {EnumSong.PATH: "hi"}, \
+            {EnumSong.PATH: "kl"}, \
+            {EnumSong.PATH: "no"}, \
+            {EnumSong.PATH: "qr"}, \
+            {EnumSong.PATH: "tu"}, \
+            {EnumSong.PATH: "wx"}, \
+    
+          ] 
+    lib3 = [
+            {EnumSong.PATH: "c"}, \
+            {EnumSong.PATH: "f"}, \
+            {EnumSong.PATH: "i"}, \
+            {EnumSong.PATH: "l"}, \
+            {EnumSong.PATH: "o"}, \
+            {EnumSong.PATH: "r"}, \
+            {EnumSong.PATH: "u"}, \
+            {EnumSong.PATH: "x"}, \
+    
+          ] 
+     
+    #for item in lib2:
+    #    print item[EnumSong.PATH],bsearch.find( item[EnumSong.PATH] )   
+    #for item in lib3:
+    #    print item[EnumSong.PATH],bsearch.find( item[EnumSong.PATH] )     
+        
+    print bsearch.find( "a" )  # none end with a      
+    print bsearch.find( "z" )  # none end with z
+        
  
