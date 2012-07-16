2ca1303976a2f35310b7bc8eaa8b74b189142685
kilik929 <kilik929@74f8f1bc-e4c4-4f01-21ac-82387f9397a1>
2012-07-16 11:48:35 +0000

    added a splashscreen to display before launch

--- trunk/src/VersionController.py
+++ trunk/src/VersionController.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python -O
-__VERSION__ = "0.6.0.108"           
+__VERSION__ = "0.6.0.112"           
 #######################################
 # do not edit anything above this line unless you 
 # know what you are doing
